1448f14c726e1f7d37715628f66a5250160f88d1
theLAN Tamer <thelantamer@gmail.com>
2019-01-24 15:40:37 -0500

    added new script

---
+++ labsync-eve-to-git.sh
@@ -0,0 +1,13 @@
+# !/bin/bash
+# labsync git to eve script
+
+# clear git labsync folder
+rm -r /git/labsync/*
+
+# copy contents of working labs folder to local repo
+cp /opt/unetlab/labs/* /git/labsync -r
+
+# update remote repo
+cd /git/labsync
+git add .
+git commit

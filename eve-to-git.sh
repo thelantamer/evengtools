2ca719a7cc34cffbad02889cb8c528f23339f327
thelantamer <thelantamer@gmail.com>
2020-01-06 14:20:21 -0500

    simplified copy command

--- eve-to-git.sh
+++ eve-to-git.sh
@@ -1,12 +1,9 @@
 # !/bin/bash
 # labsync git to eve script
 
-# clear git labsync folder
-rm -r /git/labsync/*
-
 # copy contents of working labs folder to local repo
 cp /opt/unetlab/labs/* /git/labsync -r
 
-# update remote repo
+# display git status
 cd /git/labsync
-git add .
+git status
